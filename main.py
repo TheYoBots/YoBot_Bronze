df28497c6b64afa37fb6bdfe4a43681f3acc3833
TheYoBots <elton_nathan@yahoo.com>
2021-06-04 07:43:02 +0000

    Don't sacrifice pieces when a loss is unavoidable Few other minor fixes/edits too

--- main.py
+++ main.py
@@ -27,6 +27,8 @@ def get_best_move(board: chess.Board) -> chess.Move:
         if board.is_checkmate():
             return my_candidate_move
 
+        is_my_candidate_move_attacked = __is_attacked(board, my_candidate_move.to_square)
+
         candidate_min_score = sys.maxsize
 
         for opponent_candidate_move in list(board.legal_moves):
@@ -37,15 +39,17 @@ def get_best_move(board: chess.Board) -> chess.Move:
                 current_score = -9999
             else:
                 current_score = __get_board_total_score(board, my_color)
+                if is_my_candidate_move_attacked:
+                    current_score = current_score + 1
 
-            if current_score < candidate_min_score:
-                candidate_min_score = current_score
+            candidate_min_score = min(current_score, candidate_min_score)
 
             board.pop()
 
         if candidate_min_score > global_max_score:
             global_max_score = candidate_min_score
             best_moves = [my_candidate_move]
+
         elif candidate_min_score == global_max_score:
             best_moves.append(my_candidate_move)
 
@@ -59,6 +63,26 @@ def get_best_move(board: chess.Board) -> chess.Move:
 
     return best_move
 
+
+def __is_attacked(board: chess.Board, square: chess.Square):
+
+    return len(board.attacks(square)) > 1
+
+def get_attacking_pieces(board: chess.Board, attacking_color: chess.Color, square: chess.Square) -> [chess.PieceType]:
+
+    piece_types = []
+
+    attacking_squares = board.attackers(attacking_color, square)
+
+    for attacking_square in attacking_squares:
+
+        if board.is_pinned(attacking_color, attacking_square) == False:
+            piece_type = board.piece_type_at(attacking_square)
+            piece_types.append(piece_type)
+
+    return piece_types
+
+
 def get_board_score(board: chess.Board, color: chess.Color) -> int:
 
     total = 0
@@ -70,15 +94,15 @@ def get_board_score(board: chess.Board, color: chess.Color) -> int:
     rooks = board.pieces(chess.ROOK, color)
 
     for pawn in pawns:
-        total = total + 1
+        total = total + 10
     for bishop in bishops:
-        total = total + 3
+        total = total + 30
     for knight in knights:
-        total = total + 3
+        total = total + 30
     for queen in queens:
-        total = total + 9
+        total = total + 90
     for rook in rooks:
-        total = total + 5
+        total = total + 50
 
     return total
 
